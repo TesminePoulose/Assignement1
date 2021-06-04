03f7acda37632f50da4fa6869f3c572f6fd5d326
TesminePoulose <84285771+TesminePoulose@users.noreply.github.com>
2021-06-05 02:22:06 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,54 +1,27 @@
-#!/usr/bin/env node
 "use strict";
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const app_1 = __importDefault(require("./Server/Config/app"));
-const debug_1 = __importDefault(require("debug"));
-debug_1.default('week-4b:server');
-const http_1 = __importDefault(require("http"));
-let port = normalizePort(process.env.PORT || '3000');
-app_1.default.set('port', port);
-let server = http_1.default.createServer(app_1.default);
-server.listen(port);
-server.on('error', onError);
-server.on('listening', onListening);
-function normalizePort(val) {
-    let port = parseInt(val, 10);
-    if (isNaN(port)) {
-        return val;
-    }
-    if (port >= 0) {
-        return port;
-    }
-    return false;
-}
-function onError(error) {
-    if (error.syscall !== 'listen') {
-        throw error;
-    }
-    var bind = typeof port === 'string'
-        ? 'Pipe ' + port
-        : 'Port ' + port;
-    switch (error.code) {
-        case 'EACCES':
-            console.error(bind + ' requires elevated privileges');
-            process.exit(1);
-            break;
-        case 'EADDRINUSE':
-            console.error(bind + ' is already in use');
-            process.exit(1);
-            break;
-        default:
-            throw error;
-    }
-}
-function onListening() {
-    let addr = server.address();
-    let bind = typeof addr === 'string'
-        ? 'pipe ' + addr
-        : 'port ' + addr.port;
-    debug_1.default('Listening on ' + bind);
-}
+const express_1 = __importDefault(require("express"));
+const router = express_1.default.Router();
+exports.default = router;
+router.get('/', function (req, res, next) {
+    res.render('index', { title: 'Home', page: 'home' });
+});
+router.get('/home', function (req, res, next) {
+    res.render('index', { title: 'Home', page: 'home' });
+});
+router.get('/about', function (req, res, next) {
+    res.render('index', { title: 'About Us', page: 'about' });
+});
+router.get('/projects', function (req, res, next) {
+    res.render('index', { title: 'Our Projects', page: 'projects' });
+});
+router.get('/services', function (req, res, next) {
+    res.render('index', { title: 'Our Services', page: 'services' });
+});
+router.get('/contact', function (req, res, next) {
+    res.render('index', { title: 'Contact Us', page: 'contact' });
+});
 //# sourceMappingURL=index.js.map
